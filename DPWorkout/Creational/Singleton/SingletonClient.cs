fb45a36cd15ac1c8a518a35eaea172ff684cdb98
Elvin Asadov <elvin_asadov@hotmail.com>
2018-09-22 00:36:04 +0300

    Singleton pattern added

---
+++ DPWorkout/Creational/Singleton/SingletonClient.cs
@@ -0,0 +1,10 @@
+﻿namespace DPWorkout.Creational.Singleton
+{
+    public class SingletonClient : IPatternClient
+    {
+        public void Run()
+        {
+            Clock.Instance.ShowTime();
+        }
+    }
+}
