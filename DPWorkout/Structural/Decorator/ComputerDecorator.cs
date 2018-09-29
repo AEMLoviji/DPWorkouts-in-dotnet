68313f3a7118bcd6dfd4cb071325bdb54715fa32
AEMLoviji <AEMLoviji@gmail.com>
2018-09-30 00:41:23 +0300

    decorator example added

---
+++ DPWorkout/Structural/Decorator/ComputerDecorator.cs
@@ -0,0 +1,24 @@
+﻿using DPWorkout.Structural.Decorator.Models;
+
+namespace DPWorkout.Structural.Decorator
+{
+    public abstract class ComputerDecorator : IComputer
+    {
+        protected IComputer _computer;
+
+        public ComputerDecorator(IComputer computer)
+        {
+            _computer = computer;
+        }
+
+        public void Start()
+        {
+            _computer.Start();
+        }
+
+        public void Shutdown()
+        {
+            _computer.Shutdown();
+        }
+    }
+}
---
+++ DPWorkout/Structural/Decorator/DecoratorClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DPWorkout.Structural.Decorator;
+using DPWorkout.Structural.Decorator.Models;
+
+namespace DPWorkout.Structural.Proxy
+{
+    public class DecoratorClient : IPatternClient
+    {
+        public void Run()
+        {
+            Console.WriteLine($"\t{nameof(DecoratorClient)} calls standard object");
+            IComputer computer = new Pc(Environment.MachineName);
+            computer.Start();
+            computer.Shutdown();
+
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("\t\tSwitching");
+            Console.Write(Environment.NewLine);
+            Console.WriteLine($"\t{nameof(DecoratorClient)} calls decorator object");
+
+            AdministratorComputerDecorator decorator = new AdministratorComputerDecorator(computer);
+            decorator.Start();
+            decorator.Shutdown();
+        }
+    }
+}
